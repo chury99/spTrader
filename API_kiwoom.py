dc4164ee07d156a2d1bb14a097c67db98190cbaf
chury <chury99@gmail.com>
2023-05-28 18:08:19 +0900

    키움 API 컨트롤 - 주문 및 체결/잔고 데이터 처리 모듈 추가

--- API_kiwoom.py
+++ API_kiwoom.py
@@ -20,6 +20,7 @@ import pickle
 
 
 # noinspection PyPep8Naming,PyUnresolvedReferences,PyProtectedMember,PyAttributeOutsideInit,PyArgumentList
+# noinspection PyShadowingNames
 class KiwoomAPI(QAxWidget):
     def __init__(self):
         # QAxWidget init 설정 상속
@@ -37,24 +38,30 @@ class KiwoomAPI(QAxWidget):
         # 기준정보 정의
         self.s_오늘 = pd.Timestamp('now').strftime('%Y%m%d')
         self.n_딜레이 = 0.2
-        # self.path_buysell = os.path.join(self.folder_log, f'DB_buysell_history_{s_date}.db')
-        # self.dic_data = {}
+
+        # 체결잔고 정보 불러오기
+        self.path_체결잔고 = os.path.join(self.folder_run, f'체결잔고_{self.s_오늘}.csv')
+        try:
+            self.df_체결잔고 = pd.read_csv(self.path_체결잔고, encoding='cp949')
+        except FileNotFoundError:
+            self.df_체결잔고 = pd.DataFrame()
 
         # 키움 API 연결
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
         # [ 시그널 슬랏 설정 ]
         # 로그인 이벤트
-        self.OnEventConnect.connect(self._on_event_connect)
+        self.OnEventConnect.connect(self.on_event_connect)
         # 체결 잔고 이벤트
-        # self.OnReceiveChejanData.connect(self._receive_chejan_data)
+        self.OnReceiveMsg.connect(self.on_receive_msg)
+        self.OnReceiveChejanData.connect(self.on_receive_chejan_data)
         # 조건식 종목 조회 이벤트
-        self.OnReceiveConditionVer.connect(self._on_receive_condition_ver)
-        self.OnReceiveTrCondition.connect(self._on_receive_tr_condition)
+        self.OnReceiveConditionVer.connect(self.on_receive_condition_ver)
+        self.OnReceiveTrCondition.connect(self.on_receive_tr_condition)
         # 실시간 데이터 처리 이벤트
-        # self.OnReceiveRealData.connect(self._receive_real_data)
+        self.OnReceiveRealData.connect(self.on_receive_real_data)
         # TR 처리 이벤트
-        # self.OnReceiveTrData.connect(self._receive_tr_data)
+        self.OnReceiveTrData.connect(self.on_receive_tr_data)
 
     ###################################################################################################################
 
@@ -182,6 +189,34 @@ class KiwoomAPI(QAxWidget):
 
         return s_투자유의종목
 
+    # [ 주문/체결 관련 모듈 ]
+    def send_주문(self, s_요청명, s_화면번호, s_계좌번호, n_주문유형, s_종목코드, n_주문수량, n_주문단가, s_거래구분, s_원주문번호):
+        """ 주문 요청 \n
+        SendOrder 후 3가지 이벤트 발생 (OnReceiveTRData, OnReceiveMsg, OnReceiveChejanData) """
+        # OnReceiveTRData (주문응답) - 주문발생시 첫번째 서버응답. 주문번호 취득 (주문번호가 없다면 주문거부 등 비정상주문)
+        # OnReceiveMsg (주문메세지 수신) - 주문거부 사유를 포함한 서버메세지 수신
+        # OnReceiveChejanData (주문접수/체결) - 주문 상태에 따른 실시간 수신 (주문접수, 주문체결, 잔고변경 각 단계별로 수신됨)
+
+        # [ 참고사항 ]
+        # s_계좌번호 : 10자리
+        # n_주문유형 : [1]신규매수, [2]신규매도, [3]매수취소, [4]매도취소, [5]매수정정, [6]매도정정
+        # n_주문단가 : 시장가 주문 시 주문단가 0으로 입력(상한가 기준 주문수량 자동 계산), 취소 주문 시 주문단가 0으로 입력
+        # s_거래구분 : ['00']지정가, ['03']시장가, ['05']조건부지정가, ['06']최유리지정가, ['07']최우선지정가,
+        #            ['10']지정가IOC, ['13']시장가IOC, ['16']최유리IOC, ['20']지정가FOK, ['23']시장가FOK, ['26']최유리FOK,
+        #            ['61']장전시간외종가, ['62']시간외단일가매매, ['81']장후시간외종가
+        # s_원주문번호 : 신규주문 시에는 공백
+
+        n_리턴값 = self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                    [s_요청명, s_화면번호, s_계좌번호, n_주문유형, s_종목코드, n_주문수량, n_주문단가, s_거래구분, s_원주문번호])
+
+        # 전송결과 정보 생성
+        s_주문전송 = '전송성공' if n_리턴값 == 0 else '전송실패'
+        self.s_주문_전송결과 = '주문횟수초과(초당5회)' if n_리턴값 == -308 else s_주문전송
+
+        # 이벤트 루프 생성
+        self.eventloop_주문 = QEventLoop()
+        self.eventloop_주문.exec_()
+
     # [ 조건검색 관련 모듈 ]
     def get_조건검색_전체(self):
         """ 서버에 등록되어 있는 전체 조건검색 조회하여 검색식명 별 종목코드 list 조회할 수 있는 dict 리턴 """
@@ -227,25 +262,83 @@ class KiwoomAPI(QAxWidget):
 
         return dic_조건검색_검색식명2종목코드
 
+        # [ 실시간 요청 모듈 (real_get) ]
+
+        def tset_real_reg(self, s_code, s_type):
+            ''' 실시간 데이터 감시 요청하는 함수 (장중일 때만 요청 전송, 아닐 때는 현재가 1원 리턴) '''
+            # 실시간 등록타입 설정 ('0'이면 갱신, '1'이면 추가)
+            if s_type in ['0', 'w', 'new', 'reset']:
+                s_reg_type = '0'
+            elif s_type in ['1', 'a', 'append', 'add']:
+                s_reg_type = '1'
+            else:
+                s_reg_type = '0'
+
+            # dic 초기화
+            if s_reg_type == '0':
+                self.dic_contract_real = {}
+                self.dic_hogajan_real = {}
+
+            # 장중에만 동작
+            dt_now = pd.Timestamp('now')
+            s_weekday_now = dt_now.strftime('%a')
+
+            if ((s_weekday_now in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'])
+                    and (dt_now >= pd.Timestamp('09:00:00')) and (dt_now <= pd.Timestamp('15:30:00'))):
+                # 실시간 데이터 감시 요청 (장중일 시)
+                self.set_real_reg(s_screen_no='0001', s_code=s_code, s_fid='9001;10;21;121;125', s_reg_type=s_reg_type)
+
+        def tset_real_remove(self, s_code):
+            ''' 실시간 데이터 감시 종료하는 함수 '''
+            self.set_real_remove(s_screen_no='0001', s_code=s_code)
+
+        def tget_price_real(self, s_code):
+            ''' 실시간 체결정보(누적)에서 마지막 체결가 가져와서 int로 리턴 '''
+            if s_code in self.dic_contract_real.keys():
+                # 데이터 가져오기
+                li_contract = self.dic_contract_real[s_code]
+                n_price_real = li_contract[-1][2]
+            else:
+                # 데이터 없을 시 1 리턴
+                n_price_real = 1
+
+            return n_price_real
+
+        def tget_contract_real(self, s_code):
+            ''' 실시간 체결정보(누적)을 가져와서 df로 리턴 '''
+            # 데이터 가져오기
+            li_contract = self.dic_contract_real[s_code]
+
+            # df 형식으로 정리
+            df = pd.DataFrame(li_contract, columns=['code', 'time', 'price', 'volume', 'buysell', 'cash'])
+            df['time'] = df['time'].astype('datetime64')
+            return df
+
+        def tget_hogajan_real(self, s_code):
+            ''' 실시간 호가잔량(갱신)을 가져와서 dic으로 리턴 '''
+            # 데이터 가져오기
+            dic = self.dic_hogajan_real[s_code]
+            return dic
+
+
+
     # [ TR 요청 모듈 (tr_get) ]
-
-
-    # [ 실시간 요청 모듈 (real_get) ]
-
-
-
-
-
-    def tget_ohlcv_day(self, s_code, s_date):
-        ''' 종목코드별 일봉 데이터 조회하여 df 리턴 '''
-        # TR 요청 (일봉)
-        self.set_input_value('종목코드', s_code)
-        self.set_input_value('기준일자', s_date)
+    def tr_get_일봉조회(self, s_종목코드, s_기준일=None):
+        """ 종목코드별 일봉 데이터 조회하여 df 리턴 """
+        # TR 요청
+        self.set_input_value('종목코드', s_종목코드)
+        self.set_input_value('기준일자', s_기준일)
         self.comm_rq_data('opt10081_req', 'opt10081', 0, '2001')
 
         # TR 결과 (일봉)
         ret = self.df_ohlcv_day
         return ret
+
+
+
+
+
+
 
     def tget_ohlcv_day_long(self, s_code, s_date=None):
         ''' 종목코드별 일봉 데이터 조회하여 df 리턴 '''
@@ -387,80 +480,22 @@ class KiwoomAPI(QAxWidget):
         ret = self.df_trade_history
         return ret
 
-    def tset_real_reg(self, s_code, s_type):
-        ''' 실시간 데이터 감시 요청하는 함수 (장중일 때만 요청 전송, 아닐 때는 현재가 1원 리턴) '''
-        # 실시간 등록타입 설정 ('0'이면 갱신, '1'이면 추가)
-        if s_type in ['0', 'w', 'new', 'reset']:
-            s_reg_type = '0'
-        elif s_type in ['1', 'a', 'append', 'add']:
-            s_reg_type = '1'
-        else:
-            s_reg_type = '0'
 
-        # dic 초기화
-        if s_reg_type == '0':
-            self.dic_contract_real = {}
-            self.dic_hogajan_real = {}
 
-        # 장중에만 동작
-        dt_now = pd.Timestamp('now')
-        s_weekday_now = dt_now.strftime('%a')
 
-        if ((s_weekday_now in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'])
-                and (dt_now >= pd.Timestamp('09:00:00')) and (dt_now <= pd.Timestamp('15:30:00'))):
-            # 실시간 데이터 감시 요청 (장중일 시)
-            self.set_real_reg(s_screen_no='0001', s_code=s_code, s_fid='9001;10;21;121;125', s_reg_type=s_reg_type)
 
-    def tset_real_remove(self, s_code):
-        ''' 실시간 데이터 감시 종료하는 함수 '''
-        self.set_real_remove(s_screen_no='0001', s_code=s_code)
 
-    def tget_price_real(self, s_code):
-        ''' 실시간 체결정보(누적)에서 마지막 체결가 가져와서 int로 리턴 '''
-        if s_code in self.dic_contract_real.keys():
-            # 데이터 가져오기
-            li_contract = self.dic_contract_real[s_code]
-            n_price_real = li_contract[-1][2]
-        else:
-            # 데이터 없을 시 1 리턴
-            n_price_real = 1
 
-        return n_price_real
 
-    def tget_contract_real(self, s_code):
-        ''' 실시간 체결정보(누적)을 가져와서 df로 리턴 '''
-        # 데이터 가져오기
-        li_contract = self.dic_contract_real[s_code]
 
-        # df 형식으로 정리
-        df = pd.DataFrame(li_contract, columns=['code', 'time', 'price', 'volume', 'buysell', 'cash'])
-        df['time'] = df['time'].astype('datetime64')
-        return df
 
-    def tget_hogajan_real(self, s_code):
-        ''' 실시간 호가잔량(갱신)을 가져와서 dic으로 리턴 '''
-        # 데이터 가져오기
-        dic = self.dic_hogajan_real[s_code]
-        return dic
 
-    ### 주문, 체결 관련
-    def send_order(self, s_rqname, s_screen_no, s_acc_no, n_order_type, s_code, n_quantity, n_price, s_hoga,
-                   s_order_no):
-        ''' 주문 요청 (OnReceiveChejanData 이벤트 자동 호출) '''
-        ''' [rqname]사용자, [screen_no]화면번호, [acc_no]계좌번호 10자리,
-            [order_type]주문유형 1:신규매수 2:신규매도 3:매수취소 4:매도취소 5:매수정정 6:매도정정,
-            [code]종목코드, [quantity]주문수량, [price]주문가격, [hoga]거래구분(호가구분) 00:지정가, 03:시장가,
-            [order_no]원주문번호(신규주문은 공백) '''
-        ret = self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
-                               [s_rqname, s_screen_no, s_acc_no, n_order_type, s_code, n_quantity, n_price, s_hoga,
-                                s_order_no])
-        # print(f'주문전송 요청 리턴값: {ret}')
 
-        ''' 체결잔고 데이터는 eventloop 사용 안함 (실시간 데이터 처리) '''
-        # self.eventloop_order = QEventLoop()
-        # self.eventloop_order.exec_()
 
-        ''' db파일에 저장 => self.path_buysell '''
+
+
+
+
 
 
 
@@ -485,10 +520,7 @@ class KiwoomAPI(QAxWidget):
         self.dynamicCall('SetRealRemove(QString, QString)', s_screen_no, s_code)
 
     ### TR 데이터 처리
-    def set_input_value(self, s_id, s_value):
-        ''' TR 데이터 요청항목 설정 '''
-        '''[id]아이템명-'종목코드' 등, [value]입력값-'000660' 등 '''
-        self.dynamicCall('SetInputValue(QString, QString)', s_id, s_value)
+
 
     def comm_rq_data(self, s_rqname, s_trcode, n_next, s_screen_no):
         ''' TR 데이터 요청 (OnReceiveTrData 이벤트 자동 호출) '''
@@ -521,7 +553,7 @@ class KiwoomAPI(QAxWidget):
 
     ###################################################################################################################
 
-    def _on_event_connect(self, n_에러코드):
+    def on_event_connect(self, n_에러코드):
         """ 통신 연결 결과 출력 (OnEventConnect 이벤트 연결) """
         # 리턴값 정의
         dic_에러코드 = {0: '성공', 100: '사용자 정보교환 실패', 101: '서버접속 실패', 102: '버전처리 실패'}
@@ -529,15 +561,64 @@ class KiwoomAPI(QAxWidget):
         # 접속 결과 생성
         self.s_접속결과 = dic_에러코드[n_에러코드]
 
+        # 접속 서버 확인
+        self.s_접속서버 = self.get_접속서버()
+
         # 접속 결과 출력
         if n_에러코드 == 0:
-            print('*** Connected at Kiwoom API server ***')
+            print(f'*** Connected at Kiwoom API server [{self.s_접속서버}] ***')
         else:
             print(f'*** Connection Fail - {self.s_접속결과} ***')
 
         self.eventloop_로그인.exit()
 
-    def _on_receive_condition_ver(self, n_리턴코드, s_메세지):
+    def on_receive_msg(self, s_화면번호, s_요청명, s_tr코드, s_메세지):
+        """ 주문전송 서버 메시지 수신 (OnReceiveMsg 이벤트 연결, 이벤트루프 필요) """
+        # 메세지 처리
+        self.s_주문_메세지 = s_메세지
+        print(f'메세지수신 | {s_메세지}')
+
+        # 이벤트루프 종료
+        self.eventloop_주문.exit()
+
+    def on_receive_chejan_data(self, s_구분, n_항목수, sFID목록):
+        """ 주문전송 후 주문접수, 체결통보, 잔고통보 정보 처리 (OnReceiveChejanData 이벤트 연결, FID 항목별 값 확인) """
+        # 기준정보 정의
+        dic_구분 = {'0': '주문/체결', '1': '주식잔고변경', '4': '파생잔고변경'}
+        dic_FID목록 = {'계좌번호': 9201, '주문번호': 9203, '종목코드': 9001, '주문상태': 913, '종목명': 302, '주문수량': 900,
+                     '주문가격': 901, '미체결수량': 902, '체결누계금액': 903, '원주문번호': 904, '주문구분': 905, '매매구분': 906,
+                     '매도수구분': 907, '주문체결시간': 908, '체결번호': 909, '체결가': 910, '체결량': 911, '현재가': 10,
+                     '(최우선)매도호가': 27, '(최우선)매수호가': 28, '단위체결가': 914, '단위체결량': 915, '거부사유': 919,
+                     '화면번호': 920, '신용구분': 917, '대출일': 916, '보유수량': 930, '매입단가': 931, '총매입가': 932,
+                     '주문가능수량': 933, '당일순매수수량': 945, '매도매수구분': 946, '당일총매도손일': 950, '기준가': 307,
+                     '손익율': 8019, '신용금액': 957, '신용이자': 958, '만기일': 918, '당일실현손익(유가)': 990,
+                     '당일실현손익률(유가)': 991, '당일실현손익(신용)': 992, '당일실현손익률(신용)': 993, '파생상품거래단위': 397,
+                     '상한가': 305, '하한가': 306}
+
+        # 주문/체결 정보 정리
+        df_체결잔고 = self.df_체결잔고.copy()
+        if len(df_체결잔고) == 0:
+            df_체결잔고['구분'] = [dic_구분[s_구분]]
+            for s_항목 in dic_FID목록.keys():
+                df_체결잔고[s_항목] = [self._get_chejan_data(dic_FID목록[s_항목])]
+        else:
+            df_체결잔고['구분'].append(dic_구분[s_구분])
+            for s_항목 in dic_FID목록.keys():
+                df_체결잔고[s_항목].append(self._get_chejan_data(dic_FID목록[s_항목]))
+
+        # df 정리
+        self.df_체결잔고 = pd.concat([self.df_체결잔고, df_체결잔고], axis=0)
+        self.df_체결잔고 = self.df_체결잔고.sort_values('주문체결시간', ascending=Fasle).reset_index(drop=True)
+
+        # df 저장
+        self.df_체결잔고.to_csv(self.path_체결잔고, index=False, encoding='cp949')
+
+    def _get_chejan_data(self, n_fid):
+        """ FID별 체결/잔고 데이터 요청 """
+        s_리턴값 = self.dynamicCall('GetChejanData(int)', n_fid)
+        return s_리턴값
+
+    def on_receive_condition_ver(self, n_리턴코드, s_메세지):
         """ 조건검색 목록 받아와서 해당 조건검색 결과 조회 요청 (OnReceiveConditionVer 이벤트 연결)\n
         OnReceiveTrCondition 발생 대기"""
         # 호출결과 정의
@@ -553,7 +634,7 @@ class KiwoomAPI(QAxWidget):
         # 이벤트 루프 종료
         self.eventloop_조건검색.exit()
 
-    def _on_receive_tr_condition(self, s_화면번호, s_검색종목, s_검색식명, n_검색식번호, n_연속조회여부):
+    def on_receive_tr_condition(self, s_화면번호, s_검색종목, s_검색식명, n_검색식번호, n_연속조회여부):
         """ 조건검색 결과 조회 후 체결/잔고 데이터 받아서 저장/출력 (OnReceiveTrCondition 이벤트 연결) """
         # tr 연속조회를 위한 딜레이 설정
         time.sleep(self.n_딜레이)
@@ -568,6 +649,87 @@ class KiwoomAPI(QAxWidget):
             self.eventloop_조건검색.exit()
         except AttributeError:
             pass
+
+    def on_receive_real_data(self):
+        pass
+
+
+    def set_input_value(self, s_항목명, s_설정값):
+        """ TR 요청을 위한 input 값 설정 """
+        self.dynamicCall('SetInputValue(QString, QString)', s_항목명, s_vs_설정값alue)
+
+    def on_receive_tr_data(self, s_화면번호, s_요청명, s_tr코드, s_레코드명, s_연속조회, unused1, unused2, unused3, unused4):
+        """ TR 데이터 받아오기 (OnReceiveTrData 이벤트 발생 시 연결) """
+        # [ 참고사항 ]
+        # s_연속조회 : ['0']추가 데이터 없음, ['2']추가 데이터 있음
+
+        # TR 요청에 따른 모듈 연결
+        # if s_요청명 == '주문': self._주문응답(s_요청명, s_tr코드, s_레코드명)
+
+        # # 연속 데이터 플래그 설정
+        # if s_next == '2':
+        #     self.remained_data = True
+        # else:
+        #     self.remained_data = False
+        #
+        # # TR별 데이터 리딩 함수 연결
+        # if s_rqname == 'opt10081_req': self._opt10081(s_rqname, s_trcode)  # 주식일봉차트조회 (self.df_ohlcv_day)
+        # if s_rqname == 'opw00001_req': self._opw00001(s_rqname, s_trcode)  # 예수금 현황 조회 (self.n_d2_deposit)
+        # if s_rqname == 'opw00018_req': self._opw00018(s_rqname, s_trcode)  # 계좌 평가 잔고 조회 (self.dic_balance)
+        # if s_rqname == 'opt10023_req': self._opt10023(s_rqname, s_trcode)  # 거래량급등조회요청 (self.df_volume_jump)
+        # if s_rqname == 'opt10080_req': self._opt10080(s_rqname, s_trcode)  # 주식분봉차트조회요청 (self.df_ohlcv_min)
+        # if s_rqname == 'opw00009_req': self._opw00009(s_rqname, s_trcode)  # 계좌별주문체결현황요청 (self.df_trade_history)
+        # if s_rqname == 'opt10073_req': self._opt10073(s_rqname, s_trcode)  # 일자별종목별실현손익요청 (self.df_profit)
+        # if s_rqname == 'opt20002_req': self._opt20002(s_rqname,
+        #                                               s_trcode)  # 업종별주가요청-전체종목현재가 (self.df_ohlcv_day_allcodes)
+        # if s_rqname == 'opt10001_req': self._opt10001(s_rqname, s_trcode)  # 주식기본정보요청 (액면가) (self.df_basic_inform)
+        # if s_rqname == 'opt10003_req': self._opt10003(s_rqname, s_trcode)  # 체결정보요청 (self.df_contract)
+        #
+        try:
+            self.eventloop_tr.exit()
+        except AttributeError:
+            pass
+        # pass
+
+    def _데이터길이확인(self, s_tr코드, s_요청명):
+        """ TR 결과 데이터 갯수 확인 """
+        n_리턴값 = self.dynamicCall('GetRepeatCnt(QString, QString)', s_tr코드, s_요청명)
+        return n_리턴값
+
+    def _데이터수신(self, s_tr코드, s_레코드명, n_인덱스, s_항목명):
+        """ 요청한 TR 데이터 받아오기 (OnReceiveTRData 내부에서 사용) """
+        s_리턴값 = self.dynamicCall('GetCommData(QString, QString, int, QString)', s_tr코드, s_레코드명, n_인덱스, s_항목명)
+
+        return s_리턴값.strip()
+
+    # def _주문응답(self, s_요청명, s_tr코드, s_레코드명):
+    #     """ 주문 응답 읽어오기 """
+    #
+    #     # 데이터 갯수 확인
+    #     n_데이터길이 = self._데이터길이확인(s_tr코드, s_요청명)
+    #
+    #     # 데이터 받아오기
+    #     s_주문번호 = self._데이터수신(s_tr코드, s_레코드명, 0, '주문번호')
+    #
+    #     # 이벤트 루프 종료
+    #     self.eventloop_주문.exit()
+
+        ################### 주문 들어갈 때 이 부분 확인 필요 #############################
+
+
+        # # 데이터 구조 정의
+        # dic_ohlcv_day = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': [], 'cash': []}
+        #
+        # # 데이터 받아오기
+        # for i in range(n_data_cnt):
+        #     date = self._get_comm_data(trcode, rqname, i, '일자')
+        #     open = self._get_comm_data(trcode, rqname, i, '시가')
+        #     high = self._get_comm_data(trcode, rqname, i, '고가')
+        #     low = self._get_comm_data(trcode, rqname, i, '저가')
+        #     close = self._get_comm_data(trcode, rqname, i, '현재가')
+        #     volume = self._get_comm_data(trcode, rqname, i, '거래량')
+        #     cash = self._get_comm_data(trcode, rqname, i, '거래대금')
+        # pass
 
 
 #######################################################################################################################
@@ -603,5 +765,10 @@ if __name__ == "__main__":
     # api.get_종목정보('000020')
     # api.get_투자유의종목('000020')
 
+    # api.send_주문(s_요청명='주문', s_화면번호='0001', s_계좌번호='5292685210', n_주문유형=1, s_종목코드='000020',
+    #             n_주문수량=1000, n_주문단가=1000, s_거래구분='00', s_원주문번호='')
+
     # api.get_조건검색('52주신고가')
-    api.get_조건검색_전체()
+    # api.get_조건검색_전체()
+
+    api.tr_get_일봉조회(s_종목코드='000020')
