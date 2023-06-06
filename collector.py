5edb1ffd9e02197971b8914896c7f83f813b6299
chury <chury99@gmail.com>
2023-06-06 22:19:40 +0900

    ohlcv 수집 - 전체 frame 정의

--- collector.py
+++ collector.py
@@ -28,17 +28,48 @@ class Collector:
         self.folder_캐시변환 = os.path.join(folder_데이터, '캐시변환')
         os.makedirs(self.folder_ohlcv, exist_ok=True)
         os.makedirs(self.folder_캐시변환, exist_ok=True)
+        self.folder_정보수집 = os.path.join(folder_데이터, '정보수집')
+
+        # 모니터 파일 설정
+        folder_run = os.path.join(folder_work, 'run')
+        os.makedirs(folder_run, exist_ok=True)
+        self.path_모니터 = os.path.join(folder_run, '모니터_collector.txt')
 
         # 키움 api 연결
         import API_kiwoom
         self.api = API_kiwoom.KiwoomAPI()
+        self.api.comm_connect()
 
         # log 기록
-        self.make_log(f'### Collector 구동 시작 ###')
+        self.make_log(f'### Collector 구동 시작 ({self.api.s_접속서버}) ###')
 
-    def 접속확인(self):
+    def 진행확인(self):
+        """ 전체 진행할 항목 중 얼만큼 진행되었는지 확인 후 남은 항목 진행 """
+        # 전체 항목 확인 (전체종목 pkl 파일 확인)
+        df_전체종목 = pd.read_pickle(os.path.join(self.folder_정보수집, 'df_전체종목.pkl'))
+        li_종목코드_전체 = list(df_전체종목['종목코드'].values)
+        pass
 
+        # 진행 완료한 항목 확인 (임시저장 pkl 확인)
 
+        # 진행할 항목 결정 (self 변수로 지정)
+
+        pass
+
+    def 수집_일봉(self):
+        """ 종목별 일봉 데이터 받아서 pkl 형식으로 임시 저장 """
+        pass
+
+    def 수집_분봉(self):
+        """ 종목별 분봉 데이터 받아서 pkl 형식으로 임시 저장 """
+        pass
+
+    def 변환_일봉(self):
+        """ pkl 형식으로 임시 저장된 일봉 파일 읽어와서 db, 캐시 파일 저장 """
+        pass
+
+    def 변환_분봉(self):
+        """ pkl 형식으로 임시 저장된 분봉 파일 읽어와서 db, 캐시 파일 저장 """
         pass
 
     ###################################################################################################################
@@ -66,6 +97,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     c = Collector()
 
-    c.api.comm_connect()
-    c.접속확인()
+    c.진행확인()
     pass
