1f6d9b80da4ef9d0c2b1ee04c3bfa35ad36443d9
chury <chury99@gmail.com>
2024-03-30 15:59:31 +0900

    서버정보 다운로드 - 폴더manager 이름 반영

--- collector_다운.py
+++ collector_다운.py
@@ -19,7 +19,7 @@ class Collector:
         self.n_딜레이 = 0.2
 
         # 폴더 정의
-        import UT_폴더정보
+        import UT_폴더manager
         dic_폴더정보 = UT_폴더정보.dic_폴더정보
         self.folder_정보수집 = dic_폴더정보['데이터|정보수집']
         os.makedirs(self.folder_정보수집, exist_ok=True)
